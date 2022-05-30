6785a96881c17abe5a16d924f88747a74c9ab5eb
Fuat Altekin <69749079+faltekin@users.noreply.github.com>
2022-05-30 19:50:08 +0300

    Update pratik1.java

--- pratik1.java
+++ pratik1.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 class pratik1{
 
+    // 0'dan girilen sayıya kadar olan sayılardan 3 ve 4'e tam bölünen sayıların ortalamasını hesaplayan program
+    
     public static void main(String[] args){
         int t;
         int sum = 0;
