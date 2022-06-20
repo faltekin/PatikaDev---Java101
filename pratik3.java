12b77c23913946fc82df0f47cce5f2791866c60d
Fuat Altekin <69749079+faltekin@users.noreply.github.com>
2022-06-20 20:47:38 +0300

    Update pratik3.java

--- pratik3.java
+++ pratik3.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class pratik3 {
     
+    // girilen sayıya kadar olan 4 ve 5'in kuvvetlerini ekrana yazdıran program
     
     public static void main(String[] args){ 
 
